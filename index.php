fc2b9317fc605e3c506dbc15c3c1a494d750c0c7
Tanishqa <tanishqadivyam@gmail.com>
2022-04-27 22:26:23 +0530

    index and navbar

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>todo</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+    <?php include 'nav.php';?>
+    <div class="w3-display-container" style="background-image:linear-gradient(pink,purple);min-height:80vh">
+       <div class="w3-card-4 w3-display-middle" style="background-image:linear-gradient(orange,pink);padding:8rem;border-radius:4.5rem">
+           <h1 style="font-family:cursive;font-size:6rem"><u>To-Do</u></h1>
+           <h2 style="font-family:cursive;font-size:1.5rem"><b>Keep track of the things u have to do</b></h2>
+       </div>
+    </div>
+
+</body>
+</html>
