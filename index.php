c052b30ddebf6ec894899b7690ae1f3cdc7c9f03
Tanishqa <tanishqadivyam@gmail.com>
2022-05-01 23:08:22 +0530

    login done

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <?php include 'nav.php';?>
     <div class="w3-display-container" style="background-image:linear-gradient(#defce1,#ffedfe);min-height:80vh">
        <div class="w3-card-4 w3-display-middle" style="background-image:linear-gradient(#fad7f1,#f8ffa8);padding:8rem;border-radius:4.5rem">
-           <h1 style="font-family:cursive;font-size:6rem"><u>To-Do</u></h1>
-           <h2 style="font-family:cursive;font-size:1.5rem"><b>Keep track of the things u have to do</b></h2>
+           <h1 style="font-family:cursive;font-size:7rem;margin-left:60px;"><u>To-Do</u></h1>
+           <h2 style="font-family:cursive;font-size:1.8rem;"><b>Keep track of the things u have to do</b></h2>
        </div>
     </div>
 
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+</head>
+<body style="background-color:#f7ebe6">
+    <?php include 'nav.php' ?>
+    <div class="w3-card-4 " style="max-width:600px; margin:auto; margin-top:17vh;padding:30px; border-radius:60px;background-image:linear-gradient(#e4eaf5,#f7f7dc)">
+    <h1 style="margin-left:170px;margin-top:10px; margin-bottom:20px; font-size:50px;"><b><i><u>Login</u></i></b></h1>
+    <form action="#">
+        <label style="font-family:cursive; font-size:30px;"><b><u><i class="bi-person"></i>Username</b></u></label>
+        <input type="text" placeholder="username" name="username" class="w3-input" style="margin-top:20px;">
+        <label style="font-family:cursive; font-size:30px;"><b><u><i class="bi-lock"></i>Password</b></u></label>
+        <input type="password" placeholder="password" name="passwd" class="w3-input" style="margin-top:20px;">
+        <label style="font-family:cursive; font-size:30px;"><b><u><i class="bi-check-all"></i>Confirm Password</b></u></label>
+        <input type="password" placeholder="Confirm Password" name="conpass" class="w3-input" style="margin-top:20px;">
+        <input type="submit" value="Login!" class="w3-btn w3-blue" style="padding:10px;font-size:30px;border-radius:10px;margin-top:15px;font-family:cursive;" >
+    </form>
+    </div>
+
+    
+</body>
+</html>
